7841a594500fc4df69f3d3d014ad4b696fc32fef
Marvin <marvin@mail.pollo.ai>
2025-10-20 09:45:26 +0800

    ci: nextjs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export", // ./out folder will be deployed to GitHub Pages
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
