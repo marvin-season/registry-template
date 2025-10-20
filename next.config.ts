1e00260c4a9f848d285ffe6a92197fd41d7b3888
Marvin <marvin@mail.pollo.ai>
2025-10-20 14:45:08 +0800

    feat: 优化脚本

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,18 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const isProduction = process.env.NODE_ENV === "production";
+
+const options: NextConfig = isProduction ? {
   output: "export", // ./out folder will be deployed to GitHub Pages
   images: {
     unoptimized: true,
   },
   basePath: "/registry-template",
   assetPrefix: "/registry-template/",
+} : {};
+
+const nextConfig: NextConfig = {
+  ...options
 };
 
 export default nextConfig;
