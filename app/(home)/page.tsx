9e1ed76dcf8247f5bc018776704ba60bb6ae3862
Marvin <2764876579@qq.com>
2025-10-21 19:57:25 +0800

    style: 首页优化

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,19 +1,30 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { cn } from "@/lib/utils";
+
+const linearGradient =
+  "bg-gradient-to-r from-[#6366f1] via-[#a855f7] to-[#06b6d4] text-transparent bg-clip-text";
 
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
+      <h1 className="mb-4 text-2xl font-bold">
+        <span className={"text-gray-600"}>Powered by </span>
         <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
+          target="_blank"
+          href="https://fumadocs.dev"
+          className={cn(linearGradient)}
         >
-          /docs
-        </Link>{' '}
-        and see the documentation.
-      </p>
+          Fumadocs
+        </Link>{" "}
+        <span className="text-gray-500"> & </span>
+        <Link
+          target="_blank"
+          href="https://ui.shadcn.com"
+          className={cn(linearGradient)}
+        >
+          Shadcn
+        </Link>
+      </h1>
     </main>
   );
 }
