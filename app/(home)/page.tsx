8a89671b11c288e0cbdabbd1cdda38804e8d07d7
Marvin <2764876579@qq.com>
2025-10-23 09:40:02 +0800

    fix: home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,7 +8,6 @@ export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">
-        <span className={"text-gray-600"}>Powered by </span>
         <Link
           target="_blank"
           href="https://fumadocs.dev"
