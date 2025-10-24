0c87fcf08296d61000ade0e50c73c48808572379
Marvin <2764876579@qq.com>
2025-10-24 14:20:02 +0800

    feat: 文件备注

--- .source/index.ts
+++ .source/index.ts
@@ -1,9 +1,9 @@
 // @ts-nocheck -- skip type checking
-import * as registry_2 from "../registry/new-york/blocks/hello-world/index.mdx?collection=registry&hash=1761285459455"
-import * as registry_1 from "../registry/new-york/components/zod-form/index.mdx?collection=registry&hash=1761285459455"
+import * as registry_2 from "../registry/new-york/components/zod-form/index.mdx?collection=registry&hash=1761285459455"
+import * as registry_1 from "../registry/new-york/blocks/hello-world/index.mdx?collection=registry&hash=1761285459455"
 import * as registry_0 from "../registry/hooks/use-mobile/index.mdx?collection=registry&hash=1761285459455"
 import * as docs_0 from "../content/docs/index.mdx?collection=docs&hash=1761285459455"
 import { _runtime } from "fumadocs-mdx/runtime/next"
 import * as _source from "../source.config"
 export const docs = _runtime.docs<typeof _source.docs>([{ info: {"path":"index.mdx","fullPath":"content/docs/index.mdx"}, data: docs_0 }], [{"info":{"path":"meta.json","fullPath":"content/docs/meta.json"},"data":{"title":"Project Structure","description":"Project Structure of the registry","root":true}}])
-export const registry = _runtime.docs<typeof _source.registry>([{ info: {"path":"hooks/use-mobile/index.mdx","fullPath":"registry/hooks/use-mobile/index.mdx"}, data: registry_0 }, { info: {"path":"new-york/components/zod-form/index.mdx","fullPath":"registry/new-york/components/zod-form/index.mdx"}, data: registry_1 }, { info: {"path":"new-york/blocks/hello-world/index.mdx","fullPath":"registry/new-york/blocks/hello-world/index.mdx"}, data: registry_2 }], [{"info":{"path":"new-york/meta.json","fullPath":"registry/new-york/meta.json"},"data":{"title":"Components & Blocks","description":"Components & Blocks of the registry"}}])
+export const registry = _runtime.docs<typeof _source.registry>([{ info: {"path":"hooks/use-mobile/index.mdx","fullPath":"registry/hooks/use-mobile/index.mdx"}, data: registry_0 }, { info: {"path":"new-york/blocks/hello-world/index.mdx","fullPath":"registry/new-york/blocks/hello-world/index.mdx"}, data: registry_1 }, { info: {"path":"new-york/components/zod-form/index.mdx","fullPath":"registry/new-york/components/zod-form/index.mdx"}, data: registry_2 }], [{"info":{"path":"new-york/meta.json","fullPath":"registry/new-york/meta.json"},"data":{"title":"Components & Blocks","description":"Components & Blocks of the registry"}}])
