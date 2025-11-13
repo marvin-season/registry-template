09c97220934842fab7d2af0faa9ea5a27a76ff03
Marvin <2764876579@qq.com>
2025-11-13 17:52:49 +0800

    style: 样式优化

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,31 +1,12 @@
 import Link from 'next/link';
-import { cn } from '@/lib/utils';
-
-const linearGradient = 'bg-gradient-to-r text-transparent bg-clip-text';
+import CircleShimmer from '@/components/CircleShimmer';
 
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
+    <main className="flex flex-1 flex-col justify-center items-center text-center">
       <Link href={'/docs'} className="text-2xl font-bold mb-4">
-        Start
+        <CircleShimmer />
       </Link>
-      <h1 className="mb-4 text-4xl font-bold">
-        <Link
-          target="_blank"
-          href="https://fumadocs.dev"
-          className={cn(linearGradient, 'from-[#6366f1] via-[#a855f7] to-[#06b6d4]')}
-        >
-          Fumadocs
-        </Link>{' '}
-        <span className="text-gray-500"> & </span>
-        <Link
-          target="_blank"
-          href="https://ui.shadcn.com"
-          className={cn(linearGradient, 'from-[#c3540a] via-[#b1f741] to-[#06b6d4]')}
-        >
-          Shadcn
-        </Link>
-      </h1>
     </main>
   );
 }
---
+++ src/components/CircleShimmer.tsx
@@ -0,0 +1,50 @@
+import { useMemo } from 'react';
+
+export default function CircleShimmer(props: { colorSteps?: string[]; text?: string; }) {
+  const {
+    colorSteps = ['#008aff', '#ff8a00', '#fff6d4', '#008aff', '#00ff8a', '#ff8a00'],
+    text = "Hello! I'm Marvin, a developer who is passionate about web.",
+  } = props;
+
+  const stops = useMemo(() => {
+    return colorSteps.map((color, index) => {
+      return <stop key={index} offset={`${(index * 100) / colorSteps.length}%`} stop-color={color} />;
+    });
+  }, [colorSteps]);
+
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="240" height="240" viewBox="0 0 240 240">
+      <defs>
+        <path
+          id="circlePath"
+          d="
+          M 120,120
+          m -104,0
+          a 104,104 0 1,1 208,0
+          a 104,104 0 1,1 -208,0
+        "
+        />
+
+        <linearGradient id="shineGradient" gradientUnits="userSpaceOnUse" x1="0" y1="0" x2="240" y2="0">
+          {stops}
+          <animateTransform
+            attributeName="gradientTransform"
+            type="translate"
+            from="-240 0"
+            to="240 0"
+            dur="4s"
+            repeatCount="indefinite"
+          />
+        </linearGradient>
+      </defs>
+
+      <circle cx="120" cy="120" r="104" stroke="#996" stroke-width="0" fill="none" />
+
+      <text font-size="24" fontWeight="semibold" fill="url(#shineGradient)" font-family="sans-serif">
+        <textPath href="#circlePath" startOffset="0%">
+          {text}
+        </textPath>
+      </text>
+    </svg>
+  );
+}
