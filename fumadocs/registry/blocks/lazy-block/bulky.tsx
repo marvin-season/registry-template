2465178d837e9dd2eda8385894487f13294c8541
Marvin <2764876579@qq.com>
2025-12-08 15:30:11 +0800

    fix: 删除旧的代码

--- fumadocs/registry/blocks/lazy-block/bulky.tsx
+++
@@ -1,3 +0,0 @@
-export default function Bulky() {
-  return <div className="min-h-[100px] bg-blue-500">Bulky</div>;
-}
--- fumadocs/registry/blocks/lazy-block/heaven.tsx
+++
@@ -1,3 +0,0 @@
-export default function Heaven() {
-  return <div className="min-h-[100px] bg-red-500">Heaven</div>;
-}
