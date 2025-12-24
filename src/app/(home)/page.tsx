16e9b6a666a4dc5d3f05093853785196d23f4d1f
Marvin <2764876579@qq.com>
2025-12-24 11:52:29 +0800

    feat: home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,11 +1,10 @@
 import Link from 'next/link';
-import CircleShimmer from '@/components/CircleShimmer';
 
 export default function HomePage() {
   return (
     <main className="flex flex-1 flex-col justify-center items-center text-center">
       <Link href={'/docs'} className="text-2xl font-bold mb-4">
-        <CircleShimmer />
+        Docs
       </Link>
     </main>
   );
